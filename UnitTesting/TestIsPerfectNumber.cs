6280f345d19481030cd837e8a774b480a3d2e207
Qwill9 <vladkaznin00@icloud.com>
2024-10-15 11:14:27 +0300

    bob снова родился

---
+++ UnitTesting/TestIsPerfectNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingLib.Math;
+using Xunit.Sdk;
+
+namespace UnitTesting
+{
+    public class TestIsPerfectNumber
+    {
+        public readonly BasicCalc calculator;
+        public TestIsPerfectNumber()
+        {
+            calculator = new BasicCalc();
+        }
+        [Fact]
+        public void SolveQuadraticEquationTest()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => calculator.IsPerfectNumber(-6));
+        }
+
+        [Fact]
+        public void TestIsPerfectNumber1()
+        {
+
+            bool result = calculator.IsPerfectNumber(6);
+            Assert.Equal(true, result);
+        }
+
+        [Theory]
+        [InlineData(4, false)]
+        [InlineData(6, true)]
+        [InlineData(1, false)]
+
+        public void TestIsPerfectNumber2(int a, bool expectedResult)
+        {
+            bool result = calculator.IsPerfectNumber(a);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ UnitTesting/laba7.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using TestingLib.Shop;
+using TestingLib.Weather;
+
+namespace UnitTesting
+{
+    internal class laba7
+    {
+
+    }
+}
